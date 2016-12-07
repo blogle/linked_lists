5bb94e3c984974f0b06bb8826b3c2811a5916dea
Brandon Ogle <oglebrandon@gmail.com>
2016-12-07 03:14:02 -0500

    Implement drop for linked list

--- src/first.rs
+++ src/first.rs
@@ -90,6 +90,20 @@ impl List {
     }
 }
 
+impl Drop for List {
+    fn drop(&mut self) {
+
+        let mut cur_link = mem::replace(&mut self.head, Link::Nil);
+        // while let == do this thing until this pattern fails
+        while let Link::Cons(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Nil);
+            // boxed_node goes out of scope and gets dropped here;
+            // but its Node's next field has been set to Link::Empty
+            // so no unbounded recursion occurs
+        }
+    }
+}
+
 // tests R good
 #[cfg(test)]
 mod test {
